336277e674f1089ec01c454513bd7a89b341d1e7
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-02-17 15:09:55 -0500

    adding a config.env file

--- database.js
+++ database.js
@@ -12,7 +12,7 @@ class Database {
   connect() {
     mongoose
       .connect(
-        'mongodb+srv://admin:woodpecker123@twitterclonecluster.mjlxo.mongodb.net/TwitterCloneDB?retryWrites=true&w=majority'
+        'mongodb+srv://admin:...@....mjlxo.mongodb.net/...?retryWrites=true&w=majority'
       )
       .then(() => {
         console.log('Database connection successfully'.green);
